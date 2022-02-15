cd682adb2a99f6d9cfb58cabf620e7fd0259c443
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-02-15 17:21:16 +0800

    完成数据库连接

---
+++ models/initial/Init.go
@@ -0,0 +1,32 @@
+package initial
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"main/conf"
+)
+
+//mysql,gorm配置
+var DB *gorm.DB
+
+func InitMySQL() {
+
+	dsn := conf.Config.MYSQL.Username + ":" +
+		conf.Config.MYSQL.Password + "@tcp(" +
+		conf.Config.MYSQL.Addr + ")/" +
+		conf.Config.MYSQL.Database + "?charset=utf8mb4&parseTime=True&loc=Local"
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{}) //这里用短变量声明会有歧义
+
+	if err != nil {
+		panic(err)
+	}
+
+	//绑定结构体
+	if err = DB.AutoMigrate(); err != nil {
+		log.Panicln(err)
+	}
+
+}
