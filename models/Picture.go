c1a2c322e7634d9fcae3edb5dd06cbbacfb06150
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-02-16 22:15:25 +0800

    增加头像

--- models/Picture.go
+++ models/Picture.go
@@ -3,8 +3,16 @@ package models
 //通知的图片
 type Picture struct {
 	ID          uint   `json:"id"`
-	ClubId      uint   `json:"club_id"`
+	ClubId      uint   `json:"club_id"` //对应社团的Id
 	PostId      uint   `json:"post_id"`
-	PictureName string `json:"picture_name"`
+	PictureName string `json:"picture_name"` //图片在oss的完整路径及名称
+	PictureAddr string `json:"picture_addr"` //图片的可访问地址
+}
+
+//头像
+type Avatar struct {
+	ID          uint   `json:"id"`
+	ClubId      uint   `json:"club_id"`      //对应社团的Id
+	PictureName string `json:"picture_name"` //图片在oss的完整路径及名称
 	PictureAddr string `json:"picture_addr"` //图片的可访问地址
 }
