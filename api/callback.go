e0ff919a1414d7b83882abe4000f21c4f1de6298
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-02-21 23:26:49 +0800

    回调时存入临时数据

--- api/callback.go
+++ api/callback.go
@@ -3,22 +3,43 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"log"
+	"main/models"
 	"net/http"
+	"strings"
 )
 
 type PicCallback struct {
-	PictureID   uint   `json:"picture_id" form:"picture_id" binding:"required"`
 	Size        uint64 `json:"size" form:"size" binding:"required"`
-	PictureName uint   `json:"picture_name" form:"picture_name" binding:"required"`
+	PictureName string `json:"picture_name" form:"picture_name" binding:"required"`
 }
 
-func Callback(c *gin.Context) {
-	var p PicCallback
+func GetUrlByName(name string) string {
+	return "http://incu-campus-num.ncuos.com/" + name
+}
 
-	if e := c.ShouldBind(&p); e != nil {
+//处理oss的回调内容
+func Callback(c *gin.Context) {
+	var picCallback PicCallback
+	if e := c.ShouldBind(&picCallback); e != nil {
 		log.Println(e)
+		return
+	}
+	//type
+	t := strings.Split(picCallback.PictureName, "/")[0]
+
+	if t == "avatar" {
+		var avatar models.Avatar
+		avatar.PictureName = picCallback.PictureName
+		avatar.PictureAddr = GetUrlByName(avatar.PictureName)
+		id, _ := avatar.CreatePicture()
+		c.JSON(http.StatusOK, gin.H{"200": "OK", "picture_id": id})
+		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"200": "OK", "picture_id": p.PictureID})
-
+	var picture models.PostPicture
+	picture.PictureName = picCallback.PictureName
+	picture.PictureAddr = GetUrlByName(picture.PictureName)
+	id, _ := picture.CreatePicture()
+	c.JSON(http.StatusOK, gin.H{"200": "OK", "picture_id": id})
+	return
 }
