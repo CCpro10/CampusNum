e9db139551e05c009103ded23c8ecc5719946d0b
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-02-24 17:31:22 +0800

    删除不需要的函数

--- models/Picture.go
+++ models/Picture.go
@@ -1,5 +1,6 @@
 package models
 
+//创建图片,返回图片Id
 func (p *PostPicture) CreatePicture() (newId uint, err error) {
 	DB.Create(p)
 	DB.Select("id").Last(p)
@@ -7,6 +8,7 @@ func (p *PostPicture) CreatePicture() (newId uint, err error) {
 	return newId, nil
 }
 
+//创建头像,返回图片Id
 func (p *Avatar) CreatePicture() (newId uint, err error) {
 	DB.Create(p)
 	DB.Select("id").Last(p)
@@ -14,19 +16,28 @@ func (p *Avatar) CreatePicture() (newId uint, err error) {
 	return newId, nil
 }
 
+//通过id检查图片是否为能上传的临时图片
+func CheckPostPictureById(id uint) (ok bool) {
+	var p PostPicture
+	DB.Select("post_id", "id").Where("id = ?", id).Find(&p)
+	//图片不存在
+	if p.ID == 0 {
+		return false
+	}
+	//图片不是临时图片
+	if p.PostId != 0 {
+		return false
+	}
+	return true
+}
+
 //通知或动态的图片
 type PostPicture struct {
 	ID          uint   `json:"id"`
-	ClubId      uint   `json:"club_id"`      //对应社团的Id
+	ClubId      uint   `json:"club_id"`      //对应社团的Id(主键)
 	PostId      uint   `json:"post_id"`      //帖子的Id
 	PictureName string `json:"picture_name"` //图片在oss的完整路径及名称
 	PictureAddr string `json:"picture_addr"` //图片的可访问地址
-}
-
-func GetNewPictureId() uint {
-	var p PostPicture
-	DB.Select("id").Last(&p)
-	return p.ID + 1
 }
 
 //头像
