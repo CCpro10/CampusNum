e1256829d74dbc4f5fad6fe9bffe810f690ca930
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-03-04 09:01:11 +0800

    修改路由

--- routers/routers.go
+++ routers/routers.go
@@ -5,6 +5,8 @@ import (
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"main/api"
+	"main/api/club"
+	"main/api/user"
 	_ "main/docs" //必需
 	"main/middleware"
 )
@@ -13,26 +15,31 @@ func BeginRouters(r *gin.Engine) {
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	r.Use(middleware.Cors())
-	r.POST("/register", api.Register)
-	r.GET("/login", api.Login)
+	r.POST("/register", club.Register)
+	r.GET("/login", club.Login)
 	r.POST("callback", api.Callback)
 	ClubGroup := r.Group("/club")
 	ClubGroup.Use(middleware.JWTAuthMiddleware)
 	{
 		//发通知或动态
-		ClubGroup.POST("post", api.CreatePost)
+		ClubGroup.POST("post", club.CreatePost)
 
-		ClubGroup.PUT("introduction", api.ModifyClubIntroduction)
+		ClubGroup.PUT("introduction", club.ModifyClubIntroduction)
 		//获取签名
-		ClubGroup.GET("signed_url", api.GetSignedUrl)
+		ClubGroup.GET("signed_url", club.GetSignedUrl)
 	}
 
 	UserGroup := r.Group("/user")
 	{
 		//获取单条通知或动态
-		UserGroup.GET("post", api.GetPost)
-		UserGroup.GET("club_info", api.ShowClubInfo)
-		UserGroup.GET("posts", api.GetPosts)
+		UserGroup.GET("post", user.GetPost)
+		UserGroup.GET("club_info", user.ShowClubInfo)
+		UserGroup.GET("posts", user.GetPosts)
+		UserGroup.GET("posts_from_clubs_user_fellow", user.GetPostsFromClubsUserFollow)
+		UserGroup.POST("subscribe", user.Subscribe)
+		UserGroup.DELETE("subscribe", user.UnSubscribe)
+		UserGroup.POST("collect", user.Collect)
+		UserGroup.DELETE("collect", user.CancelCollect)
 	}
 
 }
